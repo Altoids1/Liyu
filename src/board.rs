7d5adde26497f864e1a6ed5eb69fac59f142a857
Gary Lafortune <mom-puter@hotmail.com>
2022-10-07 21:49:40 -0500

    Gets board.Display() working
    Sets up basic prototypes for Piece, Tile, and BoardState, with some basic methods and ctors for them.

---
+++ src/board.rs
@@ -0,0 +1,150 @@
+//! Is all the information necessary to define a particular state of the board.
+
+pub struct BoardState
+{
+    // first dimension is x (a to i), second is y (1 to 10)
+    squares : [[Tile;9];10],
+    isRedTurn : bool,
+    plyNumber : i32 // One-indexed. Either player moving increments this. Odd for Red and even for Black
+}
+pub struct Tile
+{
+    piece : Option<Piece>
+}
+enum PieceType {
+    Pawn,
+    Advisor,
+    Elephant,
+    Horse,
+    Cannon,
+    Rook,
+    King
+}
+pub struct Piece
+{
+    pieceType : PieceType,
+    isRed : bool
+}
+
+//
+
+impl BoardState {
+    pub fn new() -> Self {
+        let mut ret =  Self {
+            squares : Default::default(),
+            isRedTurn : true,
+            plyNumber : 1
+        };
+
+        ret.loadFEN("rheakaehr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RHEAKAEHR w - - 0 1");
+
+        return ret;
+    }
+    pub fn loadFEN(&mut self, fenStr : &str) {
+        let mut x : usize = 0;
+        let mut y : usize = 9;
+        for cara in fenStr.chars() {
+            if(cara == '/') {
+                y -= 1;
+                x = 0;
+                continue;
+            }
+            if cara.is_numeric() {
+                x += cara.to_digit(10).unwrap_or(1) as usize;
+                continue;
+            }
+            match cara {
+                'p' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Pawn, isRed : false});
+                },
+                'P' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Pawn, isRed : true});
+                },
+                'a' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Advisor, isRed : false});
+                },
+                'A' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Advisor, isRed : true});
+                },
+                'e' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Elephant, isRed : false});
+                },
+                'E' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Elephant, isRed : true});
+                },
+                'h' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Horse, isRed : false});
+                },
+                'H' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Horse, isRed : true});
+                },
+                'c' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Cannon, isRed : false});
+                },
+                'C' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Cannon, isRed : true});
+                },
+                'r' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Rook, isRed : false});
+                },
+                'R' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::Rook, isRed : true});
+                },
+                'k' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::King, isRed : false});
+                },
+                'K' => {
+                    _ = self.squares[y][x].piece.insert(Piece {pieceType : PieceType::King, isRed : true});
+                },
+
+                ' ' => break, // TODO
+                _ => {}
+            }
+            x+=1;
+        }
+    }
+    pub fn Display(&self) {
+        for arr in self.squares.iter() {
+            for tile in arr {
+                if tile.piece.is_none() {
+                    print!("-");
+                    continue;
+                }
+                print!("{}",tile.piece.as_ref().unwrap().getChar());
+            }
+            print!("\n");
+        }
+    }
+}
+
+impl Tile {
+    pub fn new() -> Self {
+        return Default::default();
+    }
+}
+
+impl Default for Tile {
+    fn default() -> Self {
+        return Self {
+            piece: Default::default()
+        }
+    }
+}
+
+impl Piece {
+    pub fn getChar(&self) -> char {
+        let mut character = match self.pieceType {
+            PieceType::Pawn => 'p',
+            PieceType::Advisor => 'a',
+            PieceType::Elephant => 'e',
+            PieceType::Horse => 'h',
+            PieceType::Cannon => 'c',
+            PieceType::Rook => 'r',
+            PieceType::King => 'k'
+        };
+        if self.isRed { // red is uppercase, I've decided (goes with how chess FEN works)
+            character = character.to_uppercase().next().unwrap_or(character);
+        }
+        return character;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+#![allow(non_snake_case)] //stfu Rust jeezus
+
 use std::env;
+mod board;
 
 fn main() { 
     let args: Vec<String> = env::args().collect(); // First entry is always just like.. a relative path to where we are?
     //dbg!(args);
     println!("Liyu - Version {}",env!("CARGO_PKG_VERSION"));
+
+    let starting_board : board::BoardState = board::BoardState::new();
+    starting_board.Display();
 }
